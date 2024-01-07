838f1e713a6217f983d0d6802d818555a69f1787
Ahyoung <ahyoung227@gmail.com>
2024-01-06 23:59:58 -0500

    added dynamic routing for car detail page

---
+++ pages/cars/[id].vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>
+            <h2>Check Your Rate</h2>
+            <img :src="car.image" :alt="car.make">
+            <p>{{ car.price }}</p>
+            <p>{{ car.make }}</p>
+        </div> 
+        <NuxtLink to="/">
+            <button class="btn border-t-neutral-500">
+                <span class="text-primary text-lg">Back to the list</span>
+            </button>
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import CarCard from "~/components/CarCard.vue";
+    import { storeToRefs } from 'pinia';
+    import { useCarStore } from "../../store/car";
+    const route = useRoute();
+    const carStore = useCarStore();
+    const car = reactive(carStore.getById(parseInt(route.params.id)));
+
+</script>
+
+<style scoped>
+
+</style>
