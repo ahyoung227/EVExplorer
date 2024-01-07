e1ef4a11c751d7765b6e164c7f90ef1b960b35b0
Ahyoung <ahyoung227@gmail.com>
2024-01-07 00:00:13 -0500

    added default page

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <header class="wrapper flex justify-center">
+            <NuxtLink to="/">
+                <h1 class="text-secondary text-5xl">EVlife</h1>
+                <span class="material-symbols-outlined">location_on</span>
+            </NuxtLink>
+        </header>
+    </div>
+
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
