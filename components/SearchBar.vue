098d1d70421f89ea3b5498296e1dd1b199144248
Ahyoung <ahyoung227@gmail.com>
2024-01-07 00:00:45 -0500

    implemented search bar feature

---
+++ components/SearchBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input v-model="localState" type="text" @keypress.enter="$emit('search')">
+        <button @click="$emit('search')">Search</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const props = defineProps<({
+        modelValue: string;
+        error: Boolean
+    })>();
+
+    const emit = defineEmits<{
+        (e: 'update:modelValue', value: string): void;
+        (e: 'search') : void
+    }>();
+
+    const localState = computed({
+        get() {
+            return props.modelValue;
+        },
+        set(value) {
+            emit('update:modelValue', value);
+        }
+    })
+</script>
+    
+
+<style scoped>
+
+</style>
