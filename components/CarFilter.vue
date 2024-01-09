dbc0f2469d8b9f6958ff96c82f71f74ce0719eb6
Ahyoung <ahyoung227@gmail.com>
2024-01-08 23:53:25 -0500

    impoved sorting logic

--- components/CarFilter.vue
+++ components/CarFilter.vue
@@ -1,16 +1,33 @@
 <template>
-    <div>
-        <button class="outline outline-offset-2 outline-2" @click ="sortBy">
-            Filtered By High Incentive
-        </button>
-        <button class="outline outline-offset-2 outline-2" @click ="store.setDefaultCarData()">
-            Back to default
-        </button>
+    <div class="flex">
+        <div class="ml-auto mr-4 my-2">
+            <select v-model="selected" @change="handleSortChange" class="text-right text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+            focus:border-blue-500 block 
+            dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-600
+            dark:focus:ring-blue-500">
+            <option disabled value="">Sort By</option>
+            <option>High Incentive</option>
+            <option>Recommended</option>  
+            </select>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+    import { useCarStore, LIMIT } from "../store/car";
 
+    const store : ReturnType<typeof useCarStore> = useCarStore();
+    const selected = ref("")
+    
+    const handleSortChange : void = () => {
+        if(selected.value === "High Incentive") {
+            store.setCurOffset(LIMIT);
+            store.sortBy(selected.value);
+        } else {
+            store.setToDefaultPage()
+        }
+
+    }
 </script>
 
 <style scoped>
