e8601b5ec34e3d2035e16f8619e122c7c8836a6f
Ahyoung <ahyoung227@gmail.com>
2024-01-08 23:57:23 -0500

    fixed response nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,24 @@
 <template>
     <div>
-        <header class="wrapper flex justify-between">
-            <div class="flex">
-                <NuxtLink to="/">
-                    <h1 class="text-primary text-5xl" @click="handleDefault">
-                        <img src="assets/evlife-logo.svg" class="ml-8" alt="logo">
-                    </h1>
-                </NuxtLink>
-                <span class="material-symbols-outlined">location_on</span>                
+        <header class="wrapper sm:flex sm:justify-between sm:items-center sm:px-4">
+            <div class="flex item-center justify-between">
+                 <div>
+                    <NuxtLink to="/">
+                        <div class="text-primary text-5xl" @click="handleDefault">
+                            <img src="assets/evlife-logo.svg" class="ml-8" alt="logo">
+                        </div>
+                    </NuxtLink>              
+                </div>
+
+                <div class="sm:hidden" @click="toggleMenu">
+                    <span class="material-symbols-outlined mr-10">menu</span>
+                </div>    
             </div>
-            <div class="text-gray-400 font-light">About Us</div>
-            <div class="text-gray-400 font-light">Resources</div>
-            <div>
-                <span class="material-symbols-outlined mr-10">menu</span>
-            </div>
-            
+
+            <nav :class="menuVisible ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
+                <div class="text-gray-400 font-light mx-5 p-1">About Us</div>
+                <div class="text-gray-400 font-light mx-5 mr-8 p-1">Login</div>
+            </nav>   
         </header>
     </div>
 
@@ -24,20 +28,21 @@
 </template>
 
 <script setup lang="ts">
-    import { useCarStore } from "../store/car";
-    const store = useCarStore();
+    import { useCarStore, LIMIT } from "../store/car";
+    import { ref } from 'vue';
 
-    const { data } = await useAsyncData(
+    const store : ReturnType<typeof useCarStore> = useCarStore();
+
+    const { data } : Car[] = await useAsyncData(
         'cars',
         () => $fetch("https://freetestapi.com/api/v1/cars")
     );
 
     store.setAllCarData(data);
 
-    const handleDefault = () => {
-        store.setDefaultCarData();
-        //need to be fixed to insert offset and limit
-        store.getMoreCar(0, 6);
+    const handleDefault :void = () => {
+        store.setToDefaultPage()
+        store.getMoreCar();
     }
 
 </script>
