81010ae9c137f404954268ea32e637952de75781
Ahyoung <ahyoung227@gmail.com>
2024-01-08 23:52:30 -0500

    refactor offset logic and deleted unused actions

--- store/car.ts
+++ store/car.ts
@@ -1,5 +1,4 @@
 import { defineStore } from "pinia";
-import { v4 as uuid } from "uuid";
 
 export interface Car {
   id: number;
@@ -14,6 +13,7 @@ export interface Car {
 export interface CarState {
   cars: Car[];
   searchCars: Car[];
+  curOffset: number;
 }
 
 export interface CarAdd {
@@ -25,11 +25,14 @@ export interface CarUpdate {
   make?: string;
   horsepower: number;
 }
+//Const
+export const LIMIT : number = 6;
 
 export const useCarStore = defineStore("carStore", {
   state: (): CarState => ({
     cars: [],
-    searchCars: []
+    searchCars: [],
+    curOffset: 0
   }),
   getters: {
     getById:      
@@ -39,47 +42,41 @@ export const useCarStore = defineStore("carStore", {
     }, 
     getMoreCar: 
       (state: CarState) => 
-        (offset: number, limit: number) => {
-          return state.searchCars.slice(0, offset+limit);
-        }
+        () : Car[] => {
+          return state.searchCars.slice(0, state.curOffset);
+    },
+    getCurOffset: 
+      (state: CarState) =>
+       () : number => {
+        return state.curOffset;
+    },
   },
   actions: {
     setAllCarData(data: Car[]) {
       this.cars = data;
-      this.searchCars = this.cars.slice();
+      this.setDefaultCarData();
     },
     searchByWord(word: string) {
-      this.searchCars = this.cars.slice();
+      this.setDefaultCarData()
       this.searchCars = this.searchCars.filter((car: Car) => {
         return car.make.toLowerCase().startsWith(word.toLowerCase()) || car.model.toLowerCase().startsWith(word.toLowerCase());
       }
       );
     },
-    sortBy() {
-        this.searchCars = this.cars.slice();
+    sortBy(selected: string) {
+        this.setDefaultCarData();
         this.searchCars.sort((a: Car, b: Car) => b.horsepower - a.horsepower);
     },
     setDefaultCarData() {
       this.searchCars = this.cars.slice();
+    },
+    setCurOffset(val: number) {
+      this.curOffset = val;
+    },
+    setToDefaultPage() {
+      this.setDefaultCarData();
+      this.setCurOffset(LIMIT);
     }
-    // add(newCar: CarAdd) {
-    //   const car: Car = {
-    //     id: uuid(),
-    //     model: "model 2",
-    //     price: 1000,
-    //     mileage: 13000,
-    //     image: "",
-    //     ...newCar
-    //   };
-    //   this.cars.push(car);
-    // },
-    // remove(id: string) {
-    //   this.cars = this.cars.filter((car) => car.id != id);
-    // },
-    // update(id: string, carUpdate: CarUpdate) {
-    //   const index = this.cars.findIndex(car=> car.id === id);
-    //   this.cars[index] = { ...this.cars[index], ...carUpdate };
-    // },
   },
 });
 
