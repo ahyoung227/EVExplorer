413f823e8a394b62986b2473f43c10e8c7a9c5c6
Ahyoung <ahyoung227@gmail.com>
2024-01-08 23:55:50 -0500

    finished car detail logic

---
+++ components/UserInfoForm.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="my-10">
+        <form>
+            <div class="grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                    <label for="first_name" class="block mb-2 text-lg font-medium text-zinc-900 bold dark:text-zinc-900 bold">First name</label>
+                    <input type="text" class=" border border-gray-300 text-zinc-900 bold text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-zinc-900 bold dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required>
+                </div>
+                <div>
+                    <label for="last_name" class="block mb-2 text-lg font-medium text-zinc-900 bold dark:text-zinc-900 bold">Last name</label>
+                    <input type="text" class=" border border-gray-300 text-zinc-900 bold text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-zinc-900 bold dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required>
+                </div>
+                <div>
+                    <label for="email" class="block mb-2 text-lg font-medium text-zinc-900 bold dark:text-zinc-900 bold">Email</label>
+                    <input type="text" class=" border border-gray-300 text-zinc-900 bold text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-zinc-900 bold dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@gmail.com" required>
+                </div>  
+                <div>
+                    <label for="Credit Score" class="block mb-2 text-lg font-medium text-zinc-900 bold dark:text-zinc-900 bold">Credit Score</label>
+                    <input type="number" class=" border border-gray-300 text-zinc-900 bold text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-zinc-900 bold dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                </div>
+                <div>
+                    <label for="Household Income" class="block mb-2 text-lg font-medium text-zinc-900 bold dark:text-zinc-900 bold">Household Income</label>
+                    <input type="number" class=" border border-gray-300 text-zinc-900 bold text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-zinc-900 bold dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+                </div>
+                <div>
+                    <label for="Household Size" class="block mb-2 text-lg font-medium text-zinc-900 bold dark:text-zinc-900 bold">Household Size</label>
+                    <input type="number" class=" border border-gray-300 text-zinc-900 bold text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-zinc-900 bold dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+                </div>
+            </div>
+            <div class="flex items-start mb-6">
+                <div class="flex items-center h-5">
+                <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded  focus:ring-3 focus:ring-blue-300 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+                </div>
+                <label for="remember" class="ms-2 text-lg font-medium text-zinc-900 bold dark:text-zinc-900 bold">
+                    I'd like a free quote to bundle EV charger installation</label>
+            </div>
+            <button type="submit" class="text-white bold bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-blue-950 dark:focus:ring-blue-800">Check My Rate</button>
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/cars/[id].vue
+++ pages/cars/[id].vue
@@ -1,16 +1,20 @@
 <template>
-    <div>
-        <div>
-            <h2>Check Your Rate</h2>
-            <img :src="car.image" :alt="car.make">
-            <p>{{ car.price }}</p>
-            <p>{{ car.make }}</p>
-        </div> 
-        <NuxtLink to="/">
-            <button class="btn bg-secondary border-t-neutral-500">
-                <span class="text-lg">Back to the list</span>
-            </button>
-        </NuxtLink>
+    <div v-if="car" class="sm:flex sm:justify-center mx-6">
+        <div class="max-w-screen-x flex flex-col items-center mb-16">
+            <h2 class="my-8 text-3xl md:text-5xl text-center">Check your rate on a new EV</h2>
+            <div class="flex flex-col items-center">
+                <img :src="img.default" :alt="car.make" class="img-class w-80 h-auto">
+                <p>EV Climate Loan: {{ car.price }}</p>
+                <p>Make: {{ car.make }}</p>
+                <p>Model : {{ car.make }}</p>
+            </div>
+            <UserInfoForm />
+            <NuxtLink to="/">
+                <button type="submit" class="text-white bold bg-secondary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-1.5 text-center dark:bg-secondary dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Back to the list
+                </button>
+            </NuxtLink>
+        </div>
     </div>
 </template>
 
@@ -19,8 +23,8 @@
     import { useCarStore } from "../../store/car";
     const route = useRoute();
     const carStore = useCarStore();
-    const car = reactive(carStore.getById(parseInt(route.params.id)));
-
+    const car = reactive<Car[]>(carStore.getById(parseInt(route.params.id)));
+    const img = await import(`../../assets/mockCarImages/${car.make}.png`);
 </script>
 
 <style scoped>
